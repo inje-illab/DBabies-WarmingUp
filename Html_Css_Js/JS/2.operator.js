c580b5eaa2257af49f615df9873f4fbb6439a134
KyeongYeongDEV <yeong7098@naver.com>
2022-04-04 22:13:13 +0900

    2022.04.03

--- Html_Css_Js/JS/2.operator.js
+++ Html_Css_Js/JS/2.operator.js
@@ -38,4 +38,47 @@ console.log(ellie1 == ellie2); //false
 console.log(ellie1 === ellie2);// false   37번줄과 38번줄은 주소값이 다르기 때문에 false가 나온다.
 console.log(ellie1 === ellie3);// true  3과 1은 주소값을 공유하기 떄문에 같다
 
+// if, else if, else
+const name = 'df';
+if(name === "ellie"){
+    console.log('Wellcome, Ellie!');
+}
+else if(name === 'coder'){
+    console.log('You are amazing coder');
+}
+else{
+    console.log('unkwnon');
+}
+
+// operater ?
+console.log(name === 'ellie' ? 'yes' : "no");  //전제조건이 참일 시 yes 아닐 시 no
+
+
+//switch
+const browser = 'IE';
+switch(browser){
+    case 'IE':
+        console.log("go away!");
+        break;
+
+    case 'chrome':
+    case 'firefox': //두 가지 조건을 이어서 슬 수 있다.
+            console.log("love you!");
+            break;
+    default :
+        console.log('same all');
+        break;
+}
+
+//while
+let i =3;
+while(i >0){
+    console.log(`while : ${i}`);
+    i--;
+}
+
+//for
+for(i = 3; i >0; i--){
+    console.log(i);
+}
 
